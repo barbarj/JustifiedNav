e8fc2d501af0dd87542a8421d23b809d4c9c2b50
RJ Hallsted <rj@sideplotcreative.com>
2013-09-11 15:59:12 -0600

    First function ready
    Got findWrapperWidth working, added a bunch of css so that the look of
    the project matches that which I'm aiming for.

---
+++ js/justifiedNav.js
@@ -0,0 +1,5 @@
+//Get the width of the menu we are dealing with
+// Element -> Natural
+function findWrapperWidth( element ) {
+	return element.width();
+}
--- test-code/tests.js
+++ test-code/tests.js
@@ -1,3 +1,13 @@
-test ( "hello test", function() {
-	ok( 1 == "1", "Passed!" );
+test("findWrapperWidth", function() {
+	var $mainNav = $('.main-nav');
+	var result = findWrapperWidth( $mainNav );
+	equal( result, 960, 'Width of the "main-nav" class equals 960 pixels');
+
+	var $menu = $('header ul');
+	var result2 = findWrapperWidth( $menu );
+	equal( result2, 960, 'Width of the "header ul" equals 960 pixels');
+
+	var $menu2 = $('.menu');
+	var result3 = findWrapperWidth( $menu2 );
+	equal( result3, 960, 'Width of the "menu" class equals 960 pixels');
 });
