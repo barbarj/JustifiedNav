58b78e18b93bc68fde624f54b8c8e6da218fc022
RJ Hallsted <rj@sideplotcreative.com>
2013-09-11 17:47:53 -0600

    createRows edge cases fixed
    Added in comments for all of createRows() and tweaked code so that when
    the items width with margin right matches are max width, the script
    behaves correctly.

--- js/justifiedNav.js
+++ js/justifiedNav.js
@@ -16,37 +16,33 @@ function createRows( menu ) {
 	var workingArrayWidth = 0;
 
 	var i = 0;
-	rows[i] = [];
+	rows[i] = [];	//create the first row we are working with
 
 	$.each( $menuItems, function( key, value ) {
-		var $this = $( this );
+		var $this = $( this );	//cache the current object so we can run jquery functions on it.
 
 		workingArrayWidth += $this.outerWidth( true );
 		if( workingArrayWidth < maxRowWidth )
-			rows[i].push( $this );
+			rows[i].push( $this );	//if workingArrayWidth is less than our maxRowWidth, add the current item to the row
 
-		if ( workingArrayWidth == maxRowWidth ) {
+		/*If workingArrayWidth is greater than or equal to maxRowWidth, remove the margin from the width calculation by removing the width of the last item, and then adding it back without the margin included. If workingArrayWidth is now less than our maxWidth, add the current item to the row. If it is still more, iterate to the next row, add the item to that row, and set workingArrayWidth to the width of that item with margins included.*/
+		if ( workingArrayWidth >= maxRowWidth ) {
 			workingArrayWidth -= $this.outerWidth( true );
 			workingArrayWidth += $this.outerWidth( false );
 			if( workingArrayWidth > maxRowWidth ) {
 				i++;
-				rows.push( [] );	//create the next array in rows
+				rows.push( [] );	//create the next row
 				rows[i].push( $this );
 				workingArrayWidth = $this.outerWidth( true );
 			}
 			else {
 				rows[i].push( $this );
 				i++;
-				rows.push( [] );	//create the next array in rows
+				rows.push( [] );	//create the next row
+				workingArrayWidth = 0;
 			}
 		}
 
-		if ( workingArrayWidth > maxRowWidth ) {
-			i++;
-			rows.push( [] );	//create the next array in rows
-			rows[i].push( $this );
-			workingArrayWidth = $this.outerWidth( true );
-		}
 	});
 
 	return rows;
