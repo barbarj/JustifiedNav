310e418fe37bd47faa5682f724bfdeca9ddb36b8
RJ Hallsted <rj@sideplotcreative.com>
2013-09-11 17:57:28 -0600

    rowWidth property added
    Add a rowWidth property to each row to be later used in things that need
    the rows.

--- js/justifiedNav.js
+++ js/justifiedNav.js
@@ -22,14 +22,23 @@ function createRows( menu ) {
 		var $this = $( this );	//cache the current object so we can run jquery functions on it.
 
 		workingArrayWidth += $this.outerWidth( true );
-		if( workingArrayWidth < maxRowWidth )
+		if( workingArrayWidth < maxRowWidth ){
 			rows[i].push( $this );	//if workingArrayWidth is less than our maxRowWidth, add the current item to the row
+
+			if( key == ($menuItems.length - 1) ) {
+				//if this is the last item in menuItems, add a rowWidth property to the current row so we can use it later.
+				rows[i].rowWidth = workingArrayWidth
+			}
+		}
 
 		/*If workingArrayWidth is greater than or equal to maxRowWidth, remove the margin from the width calculation by removing the width of the last item, and then adding it back without the margin included. If workingArrayWidth is now less than our maxWidth, add the current item to the row. If it is still more, iterate to the next row, add the item to that row, and set workingArrayWidth to the width of that item with margins included.*/
 		if ( workingArrayWidth >= maxRowWidth ) {
 			workingArrayWidth -= $this.outerWidth( true );
 			workingArrayWidth += $this.outerWidth( false );
 			if( workingArrayWidth > maxRowWidth ) {
+				//add a rowWidth property to the current row so we can use it later.
+				rows[i].rowWidth = workingArrayWidth
+
 				i++;
 				rows.push( [] );	//create the next row
 				rows[i].push( $this );
@@ -37,12 +46,15 @@ function createRows( menu ) {
 			}
 			else {
 				rows[i].push( $this );
+
+				//add a rowWidth property to the current row so we can use it later.
+				rows[i].rowWidth = workingArrayWidth
+
 				i++;
 				rows.push( [] );	//create the next row
 				workingArrayWidth = 0;
 			}
 		}
-
 	});
 
 	return rows;
