d1e6ff18e7986f5869f4e7c878505ff970bbfcec
RJ Hallsted <rj@sideplotcreative.com>
2013-09-11 17:05:20 -0600

    createRows function ready.
    Built the createRows function, so we're about halfway there.

--- js/justifiedNav.js
+++ js/justifiedNav.js
@@ -1,5 +1,53 @@
 //Get the width of the menu we are dealing with
-// Element -> Natural
-function findWrapperWidth( element ) {
-	return element.width();
+// String -> Natural
+function findWrapperWidth( menu ) {
+	return $(menu).width();
+}
+
+//create an array of rows of items in the given menu
+// String -> Array
+function createRows( menu ) {
+	//create necessary initial variables
+	var rows = []
+	var maxRowWidth = findWrapperWidth( menu );
+	var $menuItems = $( menu + ' li a');
+
+	//the variable used in the .each function below to keep track of the width of our current row.
+	var workingArrayWidth = 0;
+
+	var i = 0;
+	rows[i] = [];
+
+	$.each( $menuItems, function( key, value ) {
+		var $this = $( this );
+
+		workingArrayWidth += $this.outerWidth( true );
+		if( workingArrayWidth < maxRowWidth )
+			rows[i].push( $this );
+
+		if ( workingArrayWidth == maxRowWidth ) {
+			workingArrayWidth -= $this.outerWidth( true );
+			workingArrayWidth += $this.outerWidth( false );
+			if( workingArrayWidth > maxRowWidth ) {
+				i++;
+				rows.push( [] );	//create the next array in rows
+				rows[i].push( $this );
+				workingArrayWidth = $this.outerWidth( true );
+			}
+			else {
+				rows[i].push( $this );
+				i++;
+				rows.push( [] );	//create the next array in rows
+			}
+		}
+
+		if ( workingArrayWidth > maxRowWidth ) {
+			i++;
+			rows.push( [] );	//create the next array in rows
+			rows[i].push( $this );
+			workingArrayWidth = $this.outerWidth( true );
+		}
+	});
+
+	return rows;
 }
--- test-code/tests.js
+++ test-code/tests.js
@@ -1,13 +1,21 @@
+module("Function Creation Tests");
 test("findWrapperWidth", function() {
-	var $mainNav = $('.main-nav');
-	var result = findWrapperWidth( $mainNav );
+	var mainNav = '.main-nav';
+	var result = findWrapperWidth( mainNav );
 	equal( result, 960, 'Width of the "main-nav" class equals 960 pixels');
 
-	var $menu = $('header ul');
-	var result2 = findWrapperWidth( $menu );
+	var menu = 'header ul';
+	var result2 = findWrapperWidth( menu );
 	equal( result2, 960, 'Width of the "header ul" equals 960 pixels');
 
-	var $menu2 = $('.menu');
-	var result3 = findWrapperWidth( $menu2 );
+	var menu2 = '.menu';
+	var result3 = findWrapperWidth( menu2 );
 	equal( result3, 960, 'Width of the "menu" class equals 960 pixels');
+});
+
+test('createRows', function() {
+	var mainNav = '.main-nav';
+	var actualResult = createRows( mainNav );
+	ok( actualResult );
+	console.log( actualResult );
 });
